e4ceb4e0c3b9080385068f65ff218624318633da
OmbraForza <kyanshimbi@gmail.com>
2022-05-27 09:37:39 +0200

    Shooter V1.0

---
+++ Source/Shooter/KillThemAllGameMode.h
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ShooterGameModeBase.h"
+#include "KillThemAllGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SHOOTER_API AKillThemAllGameMode : public AShooterGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	virtual void PawnKilled(APawn* PawnKilled) override;
+
+private:
+	void EndGame(bool bIsPlayerWinner);
+};
